c89d02eac19d4c35746844e1ded198a22ce3fd43
Brian Ross <brianross@outlook.com>
2021-01-02 18:05:05 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ html_context = {
 # At the bottom of conf.py
 def setup(app):
 	app.add_transform(AutoStructify)
-	app.add_js_file('https://use.fontawesome.com/6a11f6d7c9.js')
+	app.add_js_file('https://use.fontawesome.com/releases/v5.15.1/js/all.js', crossorigin='anonymous')
 	
 	
 	
